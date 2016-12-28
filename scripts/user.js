272634b38cf9eb5b7cf8ee3823dc3725667c36c4
Greg Bekher <greg@bekher.me>
2016-12-28 19:15:50 +0000

    create useradd script

---
+++ scripts/user.js
@@ -0,0 +1,61 @@
+const program = require('commander');
+const mongoose = require('mongoose');
+const User = require('../src/services/user/user-model.js');
+const crypto = require('bcryptjs');
+
+var config;
+if (process.env.NODE_ENV === 'production') {
+  config = require('../config/production.json');
+} else {
+  config = require('../config/default.json');
+}
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.mongodb);
+
+var genHash = function(password, cb) {
+  crypto.genSalt(10, function(error, salt) {
+    if (error) {
+      console.log('hash error');
+      console.log(error);
+      process.exit(1);
+    }
+    crypto.hash(password, salt, function(error, hash) {
+      if (error) {
+        console.log('hash error');
+        console.log(error);
+        process.exit(1);
+      }
+      cb(hash);
+    });
+  });
+};
+
+program
+  .command('add <username> <password> <firstname> <lastname> <group>')
+  .description('add a user')
+  .action(function(username, password, firstname, lastname, group, options) {
+    genHash(password, function(hash) {
+      var user = new User({
+        username: username,
+        password: hash,
+        firstname: firstname,
+        lastname: lastname,
+        group: group
+      });
+
+      user.save(function(err) {
+        if (!err) {
+          console.log('created user '+username);
+          process.exit(0);
+        } else {
+          console.log('mongoose error');
+          console.log(err);
+          process.exit(1);
+        }
+      });
+    });
+  });
+
+program.parse(process.argv);
