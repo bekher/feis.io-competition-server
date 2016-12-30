2c75f19a01c014c5abefe66bcd16cdbd6e35f71c
Greg Bekher <greg@bekher.me>
2016-12-30 04:20:15 +0000

    create first pass schemas

--- src/services/dancer/dancer-model.js
+++ src/services/dancer/dancer-model.js
@@ -1,19 +1,76 @@
 'use strict';
 
 // dancer-model.js - A mongoose model
-// 
-// See http://mongoosejs.com/docs/models.html
-// for more of what you can do here.
+// TODO: validate name lengths
 
 const mongoose = require('mongoose');
+const idvalidator = require('mongoose-id-validator');
 const Schema = mongoose.Schema;
 
 const dancerSchema = new Schema({
-  text: { type: String, required: true },
   createdAt: { type: Date, 'default': Date.now },
-  updatedAt: { type: Date, 'default': Date.now }
+  updatedAt: { type: Date, 'default': Date.now },
+  number: {
+    type: Number,
+    required: true
+  },
+  firstName: { 
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female', 'other']
+  },
+  externalID: { //feis.io registration id, must validate
+    type: String
+  },
+  danceInfo: [{
+    round: {
+      type: Schema.Types.ObjectId,
+      ref: 'round'
+    }, 
+    set: {
+      name: {
+        type: String,
+      },
+      speed: { // BPM
+        type: Number,
+      },
+    },
+    danceType: {
+      type: String,
+    },
+    shoeType: {
+      type: String,
+      enum: ['hard', 'soft', 'other']
+    },
+    isPresent: {
+      type: Boolean,
+      default: false
+    }
+  }],
+  /*
+  rounds: [{
+    type: Schema.Types.ObjectId,
+    ref: 'round'
+  }],
+  competitions: [{
+    type: Schema.Types.ObjectId,
+    ref: 'round'
+  }],
+  /scoresheets: [{
+  }],
+  */
+
 });
 
+dancerSchema.plugin(idvalidator);
 const dancerModel = mongoose.model('dancer', dancerSchema);
 
 module.exports = dancerModel;
--- src/services/scoresheet/scoresheet-model.js
+++ src/services/scoresheet/scoresheet-model.js
@@ -1,19 +1,60 @@
-'use strict';
+
 
 // scoresheet-model.js - A mongoose model
 // 
-// See http://mongoosejs.com/docs/models.html
-// for more of what you can do here.
+/* TODO: figure out scores subdoc,
+ *       validation
+ */
 
 const mongoose = require('mongoose');
+const idvalidator = require('mongoose-id-validator');
 const Schema = mongoose.Schema;
 
 const scoresheetSchema = new Schema({
-  text: { type: String, required: true },
   createdAt: { type: Date, 'default': Date.now },
-  updatedAt: { type: Date, 'default': Date.now }
+  updatedAt: { type: Date, 'default': Date.now },
+  feis: {
+    type: Schema.Types.ObjectId,
+    ref: 'feis',
+    required: true
+  },
+  dancer: {
+    type: Schema.Types.ObjectId,
+    ref: 'dancer',
+    required: true
+  },
+  judge: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  scoringStatus: {
+    type: String,
+    required: true,
+    enum: ['not_accessed', 'in_progress', 'complete', 'finalized'],
+    default: 'in_progress'
+  },
+  set: {
+    name: {
+      type: String,
+    },
+    speed: { //bpm
+      type: Number,
+    }
+  },
+  danceType: {
+    type: String,
+    required: true
+  },
+  shoeType: {
+    type: String,
+    enum: ['hard','soft','other'],
+    required: true,
+  }
+
 });
 
+scoresheetSchema.plugin(idvalidator);
 const scoresheetModel = mongoose.model('scoresheet', scoresheetSchema);
 
 module.exports = scoresheetModel;
